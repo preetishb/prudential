53ef428f9aefaab94b805d5dd4145c99507be901
preetish <pbakliwal@adobe.com>
2024-08-13 13:30:04 +0530

    bold css

--- blocks/calculator/calculator.js
+++ blocks/calculator/calculator.js
@@ -1,13 +1,25 @@
 export default async function decorate(block) {
-    const calcHTML = '<div class="calculator-container"><h1>Calculate the value of holding</h1><p>The current share price is <input type="number" id="sharePrice" placeholder="635.40GBP"></p><p>Enter the number of shares that you own <input type="number" id="sharesOwned" placeholder=""></p><button onclick="calculateHolding()">CALCULATE</button><p>The value of your holding currently stands at <span id="holdingValue">0GBP</span></p></div>';
+    const calcHTML = '<div class="calculator-container"><h1>Calculate the value of holding</h1><p>The current share price is <span type="number" id="sharePrice" data-shareval="635.40" class="bold-span">635.40GBP</span></p><p>Enter the number of shares that you own <input type="number" id="sharesOwned" placeholder=""></p><button class="calculateHolding">CALCULATE</button><p>The value of your holding currently stands at <span id="holdingValue" class="bold-span">0GBP</span></p></div>';
     block.innerHTML = calcHTML;
+
+    const calc = block.querySelector('.calculateHolding');
+
+    function calculateHolding() {
+        const stockPrice = parseFloat(document.getElementById("sharePrice").getAttribute('data-shareval'));
+        const sharesOwned = parseFloat(document.getElementById("sharesOwned").value);
+        const holdingValue = stockPrice * sharesOwned;
+
+        document.getElementById("holdingValue").textContent = `${holdingValue.toFixed(2)}GBP`;
+    }
+
+    calc.addEventListener(
+        'click',
+        () => {
+            calculateHolding();
+        },
+        { passive: true },
+    );
+
 }
 
 
-function calculateHolding() {
-    const stockPrice = parseFloat(document.getElementById("sharePrice").value);
-    const sharesOwned = parseFloat(document.getElementById("sharesOwned").value);
-    const holdingValue = stockPrice * sharesOwned;
-
-    document.getElementById("holdingValue").textContent = `${holdingValue.toFixed(2)}GBP`;
-}
