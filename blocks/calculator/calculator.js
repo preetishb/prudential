4efdf80455dc1475bb2ff297099672734d334774
preetish <pbakliwal@adobe.com>
2024-08-21 15:53:44 +0530

    calc cahnges

--- blocks/calculator/calculator.js
+++ blocks/calculator/calculator.js
@@ -1,5 +1,5 @@
 export default async function decorate(block) {
-    const calcHTML = '<div class="calculator-container"><h1>Calculate the value of holding</h1><p>The current share price is <span type="number" id="sharePrice" data-shareval="635.40" class="bold-span">635.40GBP</span></p><p>Enter the number of shares that you own <input type="number" id="sharesOwned" placeholder=""></p><button class="calculateHolding">CALCULATE</button><p>The value of your holding currently stands at <span id="holdingValue" class="bold-span">0GBP</span></p></div>';
+    const calcHTML = '<div class="calculator-content"><h2>Calculate the value of holding</h2><div class="calc-elem"><p>The current share price is</p> <span type="number" id="sharePrice" data-shareval="635.40" class="bold-span">635.40GBP</span></div><div class="calc-elem"><p>Enter the number of shares that you own</p> <input type="number" id="sharesOwned" placeholder=""></div><div class="calc-elem btn-wrapper"><button class="calculateHolding">CALCULATE</button></div><div class="calc-elem"><p>The value of your holding currently stands at</p> <span id="holdingValue" class="bold-span">0GBP</span></div></div>';
     block.innerHTML = calcHTML;
 
     const calc = block.querySelector('.calculateHolding');
